080c92477c6f9a31942460510d267267713ddc2f
Vyatcheslav <56754020+WCAMe@users.noreply.github.com>
2019-11-02 10:08:13 +0700

    change

--- Biquadratesolution/src/biquadratesolution/Biquadratesolution.java
+++ Biquadratesolution/src/biquadratesolution/Biquadratesolution.java
@@ -4,11 +4,11 @@ public class Biquadratesolution {
 
 
     public static void main(String[] args) {
-       BiquadraticEquation BQE = new BiquadraticEquation(1,2,1);
-       double[] Solution;
-       Solution = BQE.GetSolution();
+       BiquadraticEquation bqe = new BiquadraticEquation(1,2,1);
+       double[] solution;
+       solution = bqe.getSolution();
        for (int i = 0; i < 2;i++){
-       System.out.println(Solution[i]);
+       System.out.println(solution[i]);
        }
     }
     
--- Biquadratesolution/src/biquadratesolution/BiquadraticEquation.java
+++ Biquadratesolution/src/biquadratesolution/BiquadraticEquation.java
@@ -2,39 +2,45 @@ package biquadratesolution;
 
 
 public class BiquadraticEquation {
-    private double A, B, C;
+    private double a, b, c;
     
-public BiquadraticEquation(double a, double b, double c){
-        this.A = a;
-        this.B = b;
-        this.C = c;
-}
+    public BiquadraticEquation(double a, double b, double c){
+            this.a = a;
+            this.b = b;
+            this.c = c;
+    }
 
-private double GetDiscriminant(){
- double D;
-    D = B*B - 4*A*C;
- return D;
-}
+    private double getDiscriminant(){
+        double d;
+        d = b*b - 4*a*c;
+        return d;
+    }
 
-public double[] GetSolution(){
-    double[] Solution = new double[2];
-    double x1,x2;
-    double D = this.GetDiscriminant();
-    if (D>0){
-        x1 = (-B + Math.sqrt(D))/(2*A);
-        x2 = (-B - Math.sqrt(D))/(2*A);
-        Solution[0] = x1;
-        Solution[1] = x2;
+    public double[] getSolution(){
+        double[] solution = new double[2];
+        double x1,x2;
+        if (a == 0){
+            x1 = -c/b;
+            solution[0] = x1;
+        }
+        else{
+            double D = this.getDiscriminant();
+            if (D>0){
+                x1 = (-b + Math.sqrt(D))/(2*a);
+                x2 = (-b - Math.sqrt(D))/(2*a);
+                solution[0] = x1;
+                solution[1] = x2;
+            }
+            if (D==0){
+                x1 = (-b)/(2*a);
+                solution[0] = x1;
+            }
+            if (D<0){
+                System.out.println("При данных значениях коэффициентов A, B, C невозможно найти значение в пределах действительных чисел");
+            }
+        }
+        return solution;
     }
-    if (D==0){
-        x1 = (-B)/(2*A);
-        Solution[0] = x1;
-    }
-    if (D<0){
-        System.out.println("При данных значениях коэффициентов A, B, C невозможно найти значение в пределах действительных чисел");
-    }
-    return Solution;
-}
 
 }
 
